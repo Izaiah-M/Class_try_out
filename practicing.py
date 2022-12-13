971db4bdff6c0ec3c79b78ca808e8b31172fb5d8
iz.aia.h <imukisa024@gmail.com>
2022-12-13 21:49:41 +0300

    studies

---
+++ practicing.py
@@ -0,0 +1,97 @@
+# Linked list
+
+# Node class
+"""With the basic implementation of setting the next node, getting the next node"""
+
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+    
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, new_node):
+        self.next_node = new_node
+    
+    def __repr__(self):
+        return str(self.value)
+
+# Linked list class
+class Linked_list:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+    
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+    
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.get_head_node()
+
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += str(current_node.get_value()) + "\n"
+                current_node = current_node.get_next_node()
+            
+        return string_list
+    
+    def remove_node(self, value):
+        current_node = self.get_head_node()
+
+        if current_node.get_value() == value:
+            self.head_node = current_node.get_next_node()
+
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+
+                if next_node.get_value() == value:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
