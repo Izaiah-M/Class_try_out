91ef914781ef0b58a8484497c1c33b3ee2851f57
iz.aia.h <imukisa024@gmail.com>
2022-12-14 12:03:44 +0300

    queuess

--- practicing.py
+++ practicing.py
@@ -71,7 +71,71 @@ class Linked_list:
 # print(ll.get_head_node())
 # print(ll.stringify_list())
 
+"""QUEUES"""
 
+class Queue:
+    def __init__(self, max_size=None):
+        self.head = None
+        self.tail = None
+        self.max_size = max_size
+        self.size = 0
+    
+    def enqueue(self, new_value):
+        if self.has_space():
+            item_to_add = Node(new_value)
+            print("Adding " + str(item_to_add.get_value()))
+
+            if self.is_empty():
+                self.head = item_to_add
+                self.tail = item_to_add
+            else:
+                self.tail.set_next_node(item_to_add)
+                self.tail = item_to_add
+            self.size += 1
+        else:
+            print("Sorry no more room, we don't want a queue overflow!!")
+    
+    def dequeue(self):
+        if not self.is_empty():
+            item_to_remove = self.head
+            print("Removing " + str(item_to_remove.get_value()))
+
+            if self.get_size() == 1:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("Queue is empty and we don't want a queue underflow")
+
+
+    def get_size(self):
+        return self.size
+    
+    def has_space(self):
+        if self.max_size == None:
+            return True
+        else:
+            return self.max_size > self.get_size()
+    
+    def is_empty(self):
+        if self.get_size() == 0:
+            return True
+        else:
+            return False 
+
+    def peek(self):
+        if self.is_empty():
+            print("There is nothing to see here")
+        else:
+            return self.head.get_value()
+
+
+
+        
 
 
 
